99a718dbf2d670380efe41e4add7e5cfcede27b2
SteveL <stevelyne1971@gmail.com>
2021-05-02 18:28:02 +0200

    eighth

--- app_ny/src/components/Orders/AdminOrders.js
+++ app_ny/src/components/Orders/AdminOrders.js
@@ -4,11 +4,12 @@ import AdminOrdersList from './AdminOrdersList';
 import AdminOrderHeader from './AdminOrderHeader';
 import OrdersEmpty from './OrdersEmpty';
 import { getOrders } from '../../store/actions/ordersActions';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 
 const Orders = () => {
 
   const dispatch = useDispatch();
+  const history = useHistory();
   const id = useParams().id
 
   useEffect(() => {
@@ -21,10 +22,16 @@ const Orders = () => {
   return (
       <div id="orderMain" className="list card maxWidth">
         <div className="box d-flex justify-content-between bg-white px-2">
-          <AdminOrderHeader />
+          
           { 
-            (orders.length===0) ? <OrdersEmpty />
-            : isAuth ? orders && orders.map(order => <AdminOrdersList key={order.orderNumber} order={order} />) : <div /> 
+            (orders.length===0)
+              ? <OrdersEmpty />
+              : isAuth 
+                ? <div>
+                    <AdminOrderHeader/>
+                    { orders && orders.map(order => <AdminOrdersList key={order.orderNumber} order={order} />) }
+                  </div>
+                : history.push('/')
           }
           </div>
       </div>
